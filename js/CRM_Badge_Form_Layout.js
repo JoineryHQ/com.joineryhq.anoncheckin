9f83e6c7417d9f6aac88bc7d7a7be042e4618cf3
Allen Shaw <allen@JoineryHQ.com>
2026-09-16 17:35:57 -0500

    NFC: add code comments, cleanup whitespace.

--- js/CRM_Badge_Form_Layout.js
+++ js/CRM_Badge_Form_Layout.js
@@ -1,20 +1,27 @@
 CRM.$(function($) {
+  // Store the original value of the 'Display QR code for Anonymous QR session check-in?' option.
   var isAnoncheckinqrOriginalValue = $('input#is_anoncheckinqr').is(':checked');
-  console.log('isAnoncheckinqrOriginalValue', isAnoncheckinqrOriginalValue);
-  
+
+  // Change handler for 'Display QR code for Anonymous QR session check-in?' option.
   function isAnoncheckinqrChange() {
+    // If the 'Display QR code for Anonymous QR session check-in?' option has changed
+    // on-page, then the 'save and preview' button will not reflect that change,
+    // so disable that button.
     var currentValue = $(this).is(':checked');
     $('#_qf_Layout_refresh-bottom').attr('disabled', (currentValue != isAnoncheckinqrOriginalValue));
   }
+
   // Add id attribute to bhfe table, so it's easy to reference later.
   CRM.$('input#is_anoncheckin').closest('table').addClass('anoncheckin-bhfe-table');
 
-  CRM.$('tr.crm-badge-layout-form-block-add_barcode').before(CRM.$('input#is_anoncheckinqr').closest('tr'));  
-  
+  // Move the 'Display QR code for Anonymous QR session check-in?' option into the main form.
+  CRM.$('tr.crm-badge-layout-form-block-add_barcode').before(CRM.$('input#is_anoncheckinqr').closest('tr'));
+
   // Remove the bhfe table, but only if it's empty.
   if ($('table.anoncheckin-bhfe-table tr').length == 0) {
     $('table.anoncheckin-bhfe-table').remove();
   }
 
+  // Set change handler for 'Display QR code for Anonymous QR session check-in?' option.
   CRM.$('input#is_anoncheckinqr').change(isAnoncheckinqrChange);
 });
